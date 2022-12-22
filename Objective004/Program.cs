055ec4fbae7dc446d64b299e575f445f77a6dfbe
jntGCC <demiteli.png.@gmail.com>
2022-12-22 15:06:06 +0300

    Задача работает

--- Objective004/Program.cs
+++ Objective004/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int M = int.Parse(Console.ReadLine());
+
+if (M<B && N<B)
+{
+    Console.WriteLine($"Большее число: {B}");
+}
+else if (B<N && M<N)
+{
+    Console.WriteLine($"Большее число: {N}");
+}
+else if (B<M && N<M)
+{
+    Console.WriteLine($"Большее число: {M}");
+}
