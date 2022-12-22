e9480731fe512876eca31b843110c86e8ebd28e0
jntGCC <demiteli.png.@gmail.com>
2022-12-22 15:07:21 +0300

    Задача работает

--- Objective006/Program.cs
+++ Objective006/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else 
+{
+    Console.WriteLine("Введено нечетное число");
+}
