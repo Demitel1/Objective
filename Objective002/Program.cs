7071e531c3e10ba4b2545f5bf8c16b26f2333801
jntGCC <demiteli.png.@gmail.com>
2022-12-22 15:04:15 +0300

    добавили консоль

--- Objective002/Program.cs
+++ Objective002/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int M = int.Parse(Console.ReadLine());
+
+if (N>M)
+{
+    Console.WriteLine($"Большее число: {N}");
+}
+else if (M>N)
+{
+    Console.WriteLine($"Большее число: {M}");
+}
